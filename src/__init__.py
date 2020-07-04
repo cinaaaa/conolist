279717f3c4deb209e79559058a67a1dbcb652fa1
sina <sina.farhadi.cyber@gmail.com>
2020-07-05 00:04:00 +0430

    edit comment

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from conolist import init
 
 # use to create command line access to conolist
-def execute_from_command_line(argv=None):
+def execute_from_command_line():
     """
     Given the command-line arguments, figure out which subcommand is being
     run, create a parser appropriate to that command, and run it.
