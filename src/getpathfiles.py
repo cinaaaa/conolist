87026ad305ba5c293181442090aba434ad9608de
sina <sina.farhadi.cyber@gmail.com>
2020-07-04 18:57:32 +0430

    get path files

---
+++ src/getpathfiles.py
@@ -0,0 +1,15 @@
+"""
+ Getting all files in current path
+"""
+import os
+from os import listdir
+from os.path import isfile, join, isdir
+
+def get_files(dir):
+    
+    # get files in directory
+    files  = [f'#{f}' for f in listdir(dir) if isfile(join(dir, f))]
+    # get folders in directory
+    folders = [f'${f}' for f in listdir(dir) if isdir(join(dir, f))]
+
+    return folders + files
