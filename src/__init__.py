81452ffb4e75438f320949cf2cd1421f220b4944
sina <sina.farhadi.cyber@gmail.com>
2020-07-05 00:03:39 +0430

    ready for command access

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+from conolist import init
+
+# use to create command line access to conolist
+def execute_from_command_line(argv=None):
+    """
+    Given the command-line arguments, figure out which subcommand is being
+    run, create a parser appropriate to that command, and run it.
+    """
+    init()
---
+++ src/conolist.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from menu import menu_initilizer
+
+def init():
+    menu_initilizer()
