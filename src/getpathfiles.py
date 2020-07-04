426b0310b9b2ea19d46af814bb429a3631e554d1
sina <sina.farhadi.cyber@gmail.com>
2020-07-04 23:27:58 +0430

    change some strings

--- src/getpathfiles.py
+++ src/getpathfiles.py
@@ -8,8 +8,8 @@ from os.path import isfile, join, isdir
 def get_files(dir):
 
     # get files in directory
-    files  = [f'## {f}' for f in listdir(dir) if isfile(join(dir, f))]
+    files  = [f'XX {f}' for f in listdir(dir) if isfile(join(dir, f))]
     # get folders in directory
     folders = [f'$ {f}' for f in listdir(dir) if isdir(join(dir, f))]
 
-    return folders + files
+    return ['..'] + folders + files
